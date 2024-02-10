30bbdddeb541fec82c20ee09d5467099626dcb87
georgiibarinov <hdd.@yandex.ru>
2024-02-10 18:11:15 +0300

    новая версия

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 
 import requests
-import PyQt5
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+import io
+import sys
+import pygame
+import os
+
+mashtab = [0.6, 0.6]
+coords = [57.1, 57.1]
+
+maps_server = 'http://static-maps.yandex.ru/1.x/'
+map_params = {
+    'll': str(coords[0]) + ',' + str(coords[1]),
+    'spn': str(mashtab[0]) + ',' + str(mashtab[1]),
+    'l': 'map'}
+response = requests.get(maps_server, params=map_params)
+with open('map.png', 'wb') as f:
+    f.write(response.content)
 
 
-cords = [50, 50]
-mashtab = [0.3, 0.3]
+class SignIn(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(20, 20, 600, 450)
+        self.setWindowTitle('map')
+        self.map = QLabel(self)
+        self.map.move(0, 0)
+        self.map.resize(600, 450)
+        self.image = QPixmap('map.png')
+        self.map.setPixmap(self.image)
 
 
-map_server = 'http://static-maps.yandex.ru/1.x/'
-map_params = {'map': str(cords[0]) + ',' + str(cords[1]),
-              "mash": str(mashtab[0]) + ',' + str(cords[1]),
-              '1': "map"
-              }
-response = requests.get(map_server, params=map_params)
-with open('map.png', 'wb') as file:
-    file.write(response.content)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = SignIn()
+    ex.show()
+    sys.exit(app.exec_())
