b5dac0b53569aede7144be114aec3d356cc3135e
kockon <timofejcesakov@gmail.com>
2024-02-12 16:37:35 +0300

    Постарался сделать второе

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-import os
+import urllib3.response
 
 import requests
-from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
-import io
 import sys
-# import pygame
-# import os
-
-# mashtab = [0.6, 0.6]
-# coords = [57.1, 57.1]
-#
-# maps_server = 'http://static-maps.yandex.ru/1.x/'
-# map_params = {
-#     'll': str(coords[0]) + ',' + str(coords[1]),
-#     'spn': str(mashtab[0]) + ',' + str(mashtab[1]),
-#     'l': 'map'}
-# response = requests.get(maps_server, params=map_params)
-# with open('map.png', 'wb') as f:
-#     f.write(response.content)
+import math
 
 
 class SignIn(QMainWindow):
@@ -29,16 +14,10 @@ class SignIn(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.mashtab = 1
+        self.spn = [0.02, 0.02]
         self.coords = [30.314997, 59.938784]
-        # self.maps_server = 'http://static-maps.yandex.ru/1.x/'
-        # self.map_params = {
-        #     'll': str(self.coords[0]) + ',' + str(self.coords[1]),
-        #     'spn': str(self.mashtab) + ',' + str(self.mashtab),
-        #     'l': 'map'}
-        # self.response = requests.get(self.maps_server, params=self.map_params)
-        # with open('map.png', 'wb') as f:
-        #     f.write(self.response.content)
+        self.maps_server = 'http://static-maps.yandex.ru/1.x/'
+        self.geocode_server = 'http://geocode-maps.yandex.ru/1.x/'
         self.setGeometry(20, 20, 600, 450)
         self.setWindowTitle('map')
         self.map = QLabel(self)
@@ -48,15 +27,10 @@ class SignIn(QMainWindow):
         self.image = QPixmap('map.png')
         self.map.setPixmap(self.image)
 
-
     def update_map(self):
-        print(self.mashtab)
-        self.maps_server = 'http://static-maps.yandex.ru/1.x/'
-        self.map_params = {
-            'll': str(self.coords[0]) + ',' + str(self.coords[1]),
-            'spn': str(self.mashtab) + ',' + str(self.mashtab),
-            'l': 'map'}
-        self.response = requests.get(self.maps_server, params=self.map_params)
+        print(f'{self.maps_server}?l=map&ll={self.coords[0]}%2C{self.coords[1]}&spn={self.spn[0]}%2C{self.spn[1]}')
+        self.response = requests.get(f'{self.maps_server}?l=map&ll={self.coords[0]}%2C{self.coords[1]}&spn={self.spn[0]}%2C{self.spn[1]}')
+        # self.coordinates = requests.get(f'{self.geocode_server}?l=map')
         with open('map.png', 'wb') as f:
             f.write(self.response.content)
         self.image = QPixmap('map.png')
@@ -64,10 +38,26 @@ class SignIn(QMainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
-            self.mashtab += 0.5
+            if self.spn[0] * 2 >= 100:
+                pass
+            else:
+                self.spn[0] *= 2
+                self.spn[1] *= 2
         if event.key() == Qt.Key_PageDown:
-            self.mashtab -= self.mashtab / 10
-
+            if self.spn[0] / 2 <= 0:
+                pass
+            else:
+                self.spn[0] /= 2
+                self.spn[1] /= 2
+        elif event.key() == Qt.Key_Left:
+            self.coords[0] -= self.spn[0]
+        elif event.key() == Qt.Key_Right:
+            self.coords[0] += self.spn[0]
+        elif event.key() == Qt.Key_Up:
+            self.coords[1] += self.spn[1]
+        elif event.key() == Qt.Key_Down:
+            self.coords[1] -= self.spn[1]
+        print(self.spn)
         self.update_map()
 
 
